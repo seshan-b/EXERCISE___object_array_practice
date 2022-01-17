f1528e8d5cabc6a57690a73602544ef7d385968b
seshan-b <seshan.b@gmail.com>
2022-01-17 15:52:08 +1300

    Display the names of all of the listeners

--- tasks.js
+++ tasks.js
@@ -8,3 +8,16 @@ module.exports = {
   getReleaseByArtistGenre,
   DisplayReleaseNameOfAllListeners
 }
+
+// 1. Display the names of all of the listeners
+function displayListenerNames (arrObjFile) {
+  const listenerNames = [] // Create the empty array to push into.
+
+  for (const listener of arrObjFile) {
+    listenerNames.push(listener.name)
+    // Object File get the whole object which we are passed as a dummy param
+    // console.log(arrObjFile)
+  }
+
+  return listenerNames
+}
