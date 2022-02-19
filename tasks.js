a01c7c916ed15181957a8f9f9400b2b3bfaed51a
seshan-b <seshan.b@gmail.com>
2022-02-19 20:42:37 +1300

    Clean up

--- tasks.js
+++ tasks.js
@@ -45,7 +45,7 @@ function displayListenerNameAndGenre (arrObjFile) {
 // 3. Display the name of the pop artists
 function displayArtistNames (arrObjFile) {
   const artistNames = [] // Create the empty array to push into.
-  const artistsByGenre = arrObjFile.filter(element => element.genre === 'pop')
+  const artistsByGenre = arrObjFile.filter((element) => element.genre === 'pop')
 
   for (const artist of artistsByGenre) {
     artistNames.push(artist.name)
@@ -58,14 +58,14 @@ function displayArtistNames (arrObjFile) {
 // 4. Display the release IDs of the rap and trap artists
 
 function getId (arrObjFile) {
-  const rapArtist = arrObjFile.filter(element => element.genre === 'rap')
-  const trapArtist = arrObjFile.filter(element => element.genre === 'trap')
+  const rapArtist = arrObjFile.filter((element) => element.genre === 'rap')
+  const trapArtist = arrObjFile.filter((element) => element.genre === 'trap')
 
   const makeNewArrayArtists = [...rapArtist, ...trapArtist]
 
   console.log('Log makeNewArrayArtists: ', makeNewArrayArtists)
 
-  const ArtistId = makeNewArrayArtists.map(element => element.id)
+  const ArtistId = makeNewArrayArtists.map((element) => element.id)
 
   return ArtistId
 }
@@ -74,14 +74,18 @@ function getId (arrObjFile) {
 function releasesByArtist (artistArrObjFile, releasesArrObjFile, artistName) {
   const newArrayToLoad = []
 
-  const findArtistName = artistArrObjFile.find(element => element.name === artistName)
+  const findArtistName = artistArrObjFile.find(
+    (element) => element.name === artistName
+  )
   // console.log('Find Artist Name: ', findArtistName)
 
   const locateReleaseId = findArtistName.releaseIds
   // console.log('Release Id: ', locateReleaseId)
 
   for (let i = 0; i < locateReleaseId.length; i++) {
-    const getReleaseData = releasesArrObjFile.find(element => element.id === locateReleaseId[i])
+    const getReleaseData = releasesArrObjFile.find(
+      (element) => element.id === locateReleaseId[i]
+    )
     newArrayToLoad.push(getReleaseData)
   }
 
@@ -92,14 +96,18 @@ function releasesByArtist (artistArrObjFile, releasesArrObjFile, artistName) {
 function getGenreByArtist (listenerArrObjFile, artistArrObjFile, listenerName) {
   const newArrayToLoad = []
 
-  const findAListenerName = listenerArrObjFile.find(element => element.name === listenerName)
+  const findAListenerName = listenerArrObjFile.find(
+    (element) => element.name === listenerName
+  )
 
   const getGenre = findAListenerName.genres
 
   console.log('Get genre: ', getGenre)
 
   for (let i = 0; i < getGenre.length; i++) {
-    const getGenreData = artistArrObjFile.filter(element => element.genre === getGenre[i])
+    const getGenreData = artistArrObjFile.filter(
+      (element) => element.genre === getGenre[i]
+    )
     console.log('Log getGenre[i]', getGenre[i])
     newArrayToLoad.push(...getGenreData)
   }
@@ -107,19 +115,27 @@ function getGenreByArtist (listenerArrObjFile, artistArrObjFile, listenerName) {
 }
 
 // 7. Display the release names of Jono's genres
-function getReleaseByArtistGenre (listenerArrObjFile, releaseArrObjFile, listenerName) {
+function getReleaseByArtistGenre (
+  listenerArrObjFile,
+  releaseArrObjFile,
+  listenerName
+) {
   const newArrayToLoad = []
 
-  const findAListenerName = listenerArrObjFile.find(element => element.name === listenerName)
+  const findAListenerName = listenerArrObjFile.find(
+    (element) => element.name === listenerName
+  )
 
   const getGenre = findAListenerName.genres
 
   for (let i = 0; i < getGenre.length; i++) {
-    const getGenreData = releaseArrObjFile.filter(element => element.genre === getGenre[i])
+    const getGenreData = releaseArrObjFile.filter(
+      (element) => element.genre === getGenre[i]
+    )
     newArrayToLoad.push(...getGenreData)
   }
 
-  newArrayToLoad.map(element => element.name)
+  newArrayToLoad.map((element) => element.name)
 
   return newArrayToLoad
 }
